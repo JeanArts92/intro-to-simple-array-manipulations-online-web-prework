b5a1df94e31e6f56626ea44b2a0177fbb34cd397
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 19:51:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,9 @@
-def using_push(countries_in_western_africa,"Next Country")
+def using_push(countries_in_western_africa, push)
   countries_in_western_africa.push("Niger")
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, "new neighborhood")
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
@@ -10,3 +11,7 @@ def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop
 end
 
+def using_pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+  chars_arya_killed = chars_in_game_of_thrones.pop(3)
+end
