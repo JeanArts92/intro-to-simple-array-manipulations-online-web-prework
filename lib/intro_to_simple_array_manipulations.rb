b2a19e3563a48203488f4e9da5804299c7136b1a
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 19:48:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,9 @@
-def using_push(countries_in_western_africa)
+def using_push(countries_in_western_africa, )
   countries_in_western_africa.push("Niger")
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn)
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
