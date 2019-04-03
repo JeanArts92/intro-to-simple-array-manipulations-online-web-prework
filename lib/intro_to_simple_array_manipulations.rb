d943770ba3684cf5596e1374ef3bc4411a7447be
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 19:51:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,6 @@ def using_push(countries_in_western_africa, push)
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
