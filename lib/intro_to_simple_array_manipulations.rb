cf60e4b8d04e13f56113ec818a7984277a3d4fd6
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 20:07:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
 
-def using_concat(my_favorite_things, more_faves)
+def using_concat(my_favorite_things
   my_favorite_things + more_faves
 end
   
