c21c1c225fededf684bc738256a0e32eeabd91aa
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 19:37:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,7 +8,7 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
-def using_pop(great_hits_of_the_nineties)
+def using_pop
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   great_hits_of_the_nineties.pop
 end
