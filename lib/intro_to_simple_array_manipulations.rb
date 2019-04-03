0b0b6545fde83502a8bb00414344eaafde0c1199
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 20:43:19 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,53 @@
 def using_push(countries_in_western_africa, push)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   countries_in_western_africa.push("Niger")
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
-def using_pop(great_hits_of_the_nineties,)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop
 end
 
 def using_pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-  chars_in_game_of_thrones.pop
+  chars_arya_killed = chars_in_game_of_thrones.pop(3)
 end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+  
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift 
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, more_faves)
+  my_favorite_things.concat(more_faves)
+end
+
+def using_insert(list_of_esoteric_programming_languages,another_esoteric_language)
+  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+end
+
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers.uniq 
+end
+
+def using_flatten(flat_array)
+  flat_array.flatten 
+end
+
+def using_delete(instructors, no_offense_steven)
+  instructors.delete("Steven")
+end
+
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
+end
+
