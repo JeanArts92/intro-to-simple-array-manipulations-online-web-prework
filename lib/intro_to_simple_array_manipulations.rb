5b04147bfef0169a1fd129731903cb5ae027d4b5
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-03 18:39:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,7 +8,7 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
-def using_pop(great_hits_of_the_nineties,)
+def using_pop(great_hits_of_the_nineties)
  
 end
 
